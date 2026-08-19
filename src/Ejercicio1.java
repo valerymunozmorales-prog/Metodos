8d70346d76dae8ea167e9009e4186529219f7495
Valery Muñoz Morales <valerymunozmorales@gmail.com>
2026-08-19 16:16:33 -0500

    Realice el Ejercicio #1

---
+++ src/Ejercicio1.java
@@ -0,0 +1,13 @@
+public class Ejercicio1 {
+    public static void mostrarBienvenida() {
+        System.out.println("Bienvenido al instituto");
+        }
+
+        public static void main(String[] args) {
+               mostrarBienvenida();
+
+            }
+        }
+
+
+
