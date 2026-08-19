56c284c8c6d01bbf6a19cde5498944e26899cf53
Valery Muñoz Morales <valerymunozmorales@gmail.com>
2026-08-19 16:33:02 -0500

    Realice el ejercicio #2

---
+++ src/Ejercicio2.java
@@ -0,0 +1,12 @@
+public class Ejercicio2 {
+    public static void saludarEstudiante(String nombre, int edad) {
+
+        System.out.println("Hola " + nombre + " tienes " + edad + " años.");
+    }
+
+    public static void main(String[] args) {
+        saludarEstudiante("Santiago", 15);
+
+    }
+}
+
