4a94dd7f7151be5f0c2be306f8fabfdb5c6784d8
Valery Muñoz Morales <valerymunozmorales@gmail.com>
2026-08-19 16:44:17 -0500

    Realice el ejercicio #3

---
+++ src/Ejercicio3.java
@@ -0,0 +1,11 @@
+public class Ejercicio3 {
+    public static double calcularAreaRectangulo (double base, double altura){
+
+        return (base*altura);
+    }
+    public static void main (String[] args){
+        double area = calcularAreaRectangulo(5.0, 3.0);
+        System.out.println(area);
+
+    }
+}
